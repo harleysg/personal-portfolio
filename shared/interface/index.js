69ce257d2b2f72be2486442338906af21a26a224
harley santos garzon <harley.santos.garzon@gmail.com>
2021-09-21 00:08:19 -0500

    feat Root add pseudo interface

--- shared/interface/index.js
+++ shared/interface/index.js
@@ -9,3 +9,37 @@ export const ICard = {
     height: 0
   }
 };
+
+export const IMeta = {
+  title: '',
+  descriptions: ''
+}
+
+export const IMenu = {
+  link: '',
+  text: ''
+}
+export const IHeading = {
+  indicator: '',
+  content: '',
+  enfasis: '',
+  space: false,
+}
+
+export const ISection = {
+  IHeading,
+  megaHeadding: '',
+  data: {}
+}
+
+export const IData = {
+  meta: IMeta,
+  menu: IMenu,
+  sections: {
+    welcome: ISection,
+    guide_typo: ISection,
+    guide_card: ISection,
+    guide_gallery: ISection,
+    guide_form: ISection,
+  }
+}
