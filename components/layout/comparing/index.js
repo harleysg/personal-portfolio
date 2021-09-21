1bd2116b902a18e03281a6eebe15eaf3d3246e65
harley santos garzon <harley.santos.garzon@gmail.com>
2021-09-21 00:08:28 -0500

    feat Component new Compare

---
+++ components/layout/comparing/index.js
@@ -0,0 +1,8 @@
+export default function Compare({ left, right }) {
+  return (
+    <article className={'c-compare'}>
+      <div className={'c-compare_left'}>{left}</div>
+      <div className={'c-compare_right'}>{right}</div>
+    </article>
+  );
+}
