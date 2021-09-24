6852e4e28357b413588c40499964a4bf3f123411
harley santos garzon <harley.santos.garzon@gmail.com>
2021-09-24 00:45:04 -0500

    refactor headdingAccent remove hook useNearScreen

--- components/layout/headdingAccent/index.js
+++ components/layout/headdingAccent/index.js
@@ -1,5 +1,4 @@
 import { useRef, useEffect } from 'react';
-import useNearScreen from 'hooks/useNearScreen';
 
 import css from './index.module.scss';
 
@@ -12,24 +11,17 @@ function _getScrollTop() {
 
 export default function MegaHeadding({ text }) {
   const HeaddingRef = useRef(null);
-  const { isNearScreen } = useNearScreen({
-    extRef: HeaddingRef,
-    once: false,
-    distance: `${100}px`
-  });
 
   useEffect(() => {
     function handleHeaddingMovement() {
       const scrollSize = _getScrollTop();
       const mega_headding = HeaddingRef.current;
 
-      if (isNearScreen) {
-        setTimeout(() => {
-          mega_headding
-            ? (mega_headding.style.left = `-${scrollSize / 3}px`)
-            : null;
-        }, 90);
-      }
+      setTimeout(() => {
+        mega_headding
+          ? (mega_headding.style.left = `-${scrollSize / 3}px`)
+          : null;
+      }, 90);
     }
 
     document.addEventListener('scroll', handleHeaddingMovement);
@@ -37,7 +29,7 @@ export default function MegaHeadding({ text }) {
     return () => {
       document.removeEventListener('scroll', handleHeaddingMovement);
     };
-  }, [isNearScreen]);
+  }, []);
   return (
     <div className={`${css['c-mega_headding-wrapper']}`}>
       <div className={`${css['c-mega_headding']}`} ref={HeaddingRef}>{text}</div>
