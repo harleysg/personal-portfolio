42513d54d27cd7647b308289d0b4ec040748a005
harley santos garzon <harley.santos.garzon@gmail.com>
2021-10-27 00:33:11 -0500

    refactor layout/Section add props

--- components/layout/section/index.js
+++ components/layout/section/index.js
@@ -1,11 +1,17 @@
 export default function Section (props) {
-  const { children, theme, id, hasWrapper = false } = props
+  const { children, theme, id, hasWrapper = false, intermedian } = props
 
   const HasWrapper = ({ children }) =>
     hasWrapper ? <div className='o-wrapper'>{children}</div> : <>{children}</>
 
   return (
-    <section id={id} className='o-section' data-theme={theme}>
+    <section
+      id={id}
+      className='o-section'
+      data-theme={theme}
+      data-intermedian={intermedian}
+      {...props}
+    >
       <HasWrapper>{children}</HasWrapper>
     </section>
   )
