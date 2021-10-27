616be8cc0c130965312ff5e2de93131f5887eae6
harley santos garzon <harley.santos.garzon@gmail.com>
2021-10-27 00:32:06 -0500

    refactor Slider Improve slider and replace slide package

--- components/slider/index.js
+++ components/slider/index.js
@@ -1,102 +1,37 @@
-import Image from 'next/image'
-import { useEffect, useState } from 'react'
+import { Children, useEffect, useState } from 'react'
 
-import SimpleImageSlider from 'react-simple-image-slider'
 import ReactSlidy from 'react-slidy'
 import useMatchMedia from '../../hooks/useMatchMedia'
-import { IMediaQueryList } from '@/interface/index'
-import styles from './slider.module.scss'
-
-const initialPaddingBottom = '56.25%'
-const defaultProps = {
-  styles: {},
-  mediaQuery: IMediaQueryList,
-  paddingBottom: '72%',
-  images: [],
-  others: {},
-  className: ''
-}
-
-function SlideWrapper ({ children }) {
-  return <div>{children}</div>
-}
-
-function SlideImage (props) {
-  const { img, className, text } = props
-
-  return (
-    <div
-      className={`${styles['c-slider_custom']}${
-        className ? ` ${className}` : ''
-      }`}
-    >
-      <Image alt='' src={img.url} width={1141} height={644} />
-      <button>{text}</button>
-    </div>
-  )
-}
+import { ISlider } from '@/interface/index'
 
 export default function Slider ({
-  styles = defaultProps.styles,
-  mediaQuery = IMediaQueryList,
-  paddingBottom = defaultProps.paddingBottom,
-  images = defaultProps.images,
-  others = defaultProps.others,
-  className = defaultProps.className
+  mediaQuery = ISlider.mediaQuery,
+  className = ISlider.className,
+  children = null,
+  useMediaQuery = false,
+  ...props
 }) {
-  const isMobileSize = useMatchMedia(mediaQuery)
-  const [customRootSliderStyle, setCustomRootSliderStyle] = useState(() => {
-    return { ...styles, paddingBottom }
-  })
+  const isMobileSize = useMatchMedia(useMediaQuery, mediaQuery)
   const [onMobileClass, setOnMobileClass] = useState(false)
+  const slides = Children.count(children)
 
   useEffect(() => {
     if (isMobileSize.match) {
-      setCustomRootSliderStyle((styles) => ({
-        ...styles,
-        paddingBottom: paddingBottom || initialPaddingBottom
-      }))
       setOnMobileClass('isMobile')
     } else {
-      setCustomRootSliderStyle((styles) => ({
-        ...styles,
-        paddingBottom: initialPaddingBottom
-      }))
       setOnMobileClass('notMobile')
     }
-  }, [setCustomRootSliderStyle, isMobileSize, paddingBottom, setOnMobileClass])
+  }, [isMobileSize, setOnMobileClass])
 
-  if (images && images.length) {
+  if (slides) {
     return (
-      <SlideWrapper className={className}>
-        {
-        others.slideOption
-          ? (
-            <SimpleImageSlider
-              width='100%'
-              height='100%'
-              images={images}
-              showBullets
-              showNavs
-              style={customRootSliderStyle}
-              {...others}
-            />
-            )
-          : (
-            <ReactSlidy>
-              {images.map((img) => (
-                <SlideImage
-                  key={img}
-                  img={img}
-                  text='View more'
-                  className={`${onMobileClass} ${className}`}
-                />
-              ))}
-            </ReactSlidy>)
-        }
-      </SlideWrapper>
+      <div className={className.concat(` ${onMobileClass}`)}>
+        <ReactSlidy lazyLoadSlider={false} showArrows {...props}>
+          {
+            children
+          }
+        </ReactSlidy>
+      </div>
     )
-  } else {
-    return <></>
-  }
+  } else { return <></> }
 }
