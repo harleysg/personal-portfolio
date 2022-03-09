353acc72cfd5d053f148f0fcfdd58dc00fd76aa9
harley santos garzon <harley.santos.garzon@gmail.com>
2022-03-09 02:12:32 -0500

    refactor(components): replace useEffect by hook

--- components/layout/headdingAccent/index.js
+++ components/layout/headdingAccent/index.js
@@ -1,36 +1,15 @@
-import { useRef, useEffect } from 'react'
+import { useRef } from 'react'
+import useLeftTransitionByScroll from '@/hooks/useLeftTransitionByScroll'
 
 import css from './index.module.scss'
 
-function _getScrollTop () {
-  return window.pageYOffset !== undefined
-    ? window.pageYOffset
-    : (document.documentElement || document.body.parentNode || document.body)
-        .scrollTop
-}
-
 export default function MegaHeadding ({ text }) {
   const HeaddingRef = useRef(null)
+  useLeftTransitionByScroll({ ref: HeaddingRef })
 
-  useEffect(() => {
-    function handleHeaddingMovement () {
-      const scrollSize = _getScrollTop()
-      const megaHeadding = HeaddingRef.current
-
-      setTimeout(() => {
-        megaHeadding && (megaHeadding.style.left = `-${scrollSize / 3}px`)
-      }, 90)
-    }
-
-    document.addEventListener('scroll', handleHeaddingMovement)
-
-    return () => {
-      document.removeEventListener('scroll', handleHeaddingMovement)
-    }
-  }, [])
   return (
     <div className={`${css['c-mega_headding-wrapper']}`}>
-      <div className={`${css['c-mega_headding']}`} ref={HeaddingRef}>{text}</div>
+      <div className={`${css['c-mega_headding']}`} ref={ HeaddingRef }>{text}</div>
     </div>
   )
 }
